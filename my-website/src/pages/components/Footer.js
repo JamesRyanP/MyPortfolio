ee8583af698c2135e1effe5a8ebd52e222760bc9
Your Name <James.Patzmann@gmail.com>
2024-02-08 18:28:23 -0500

    arcade portfoli page fix

--- my-website/src/pages/components/Footer.js
+++ my-website/src/pages/components/Footer.js
@@ -1,23 +1,26 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link from React Router
 import './styling/Footer.css';
-import { ReactComponent as Linkedin } from '../../assets/icons/linkedin_icon.svg';
-import { ReactComponent as Github } from '../../assets/icons/github_icon.svg';
-import { ReactComponent as Youtube } from '../../assets/icons/youtube_icon.svg';
-
-
+import { ReactComponent as LinkedinIcon } from '../../assets/icons/linkedin_icon.svg';
+import { ReactComponent as GithubIcon } from '../../assets/icons/github_icon.svg';
+import { ReactComponent as YoutubeIcon } from '../../assets/icons/youtube_icon.svg';
 
 const Footer = () => {
-
-
   return (
     <header className="footer">
       <nav className="footer-links">
-        <Linkedin alt="Logo" className="social-media" id="linkedin" />
-        <Github alt="Logo" className="social-media" id="github" />
-        <Youtube alt="Logo" className="social-media" id="youtube" />
+        <Link to="https://www.linkedin.com/in/jamespatzmann" target="_blank" rel="noopener noreferrer">
+          <LinkedinIcon alt="LinkedIn" className="social-media" id="linkedin" />
+        </Link>
+        <Link to="https://github.com/JamesRyanP" target="_blank" rel="noopener noreferrer">
+          <GithubIcon alt="GitHub" className="social-media" id="github" />
+        </Link>
+        <Link to="https://www.youtube.com/channel/UCBn6GSBh80jJrqADdXZY4xA" target="_blank" rel="noopener noreferrer">
+          <YoutubeIcon alt="YouTube" className="social-media" id="youtube" />
+        </Link>
       </nav>
     </header>
   );
 };
-  
-  export default Footer;
+
+export default Footer;
